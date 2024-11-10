eff822d1100da4fb7b8e107d5335b2ec28043459
PiyushJT <piyushthummar7@gmail.com>
2024-11-10 14:56:15 +0530

    pattern 4

---
+++ Loops/Pattern295.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Pattern295 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        int no = 0;
+
+        for(int i = 1; i <= n; i++){
+
+            System.out.println();
+
+            if(i%2 == 1){
+                no = 1;
+            }
+            else{
+                no = 0;
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print(no);
+                no = (no==1)?0:1;
+            }
+
+        }
+
+    }
+}
