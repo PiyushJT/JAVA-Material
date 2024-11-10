3572d1cbb86836218fc0086915c88468ff6f71fa
PiyushJT <piyushthummar7@gmail.com>
2024-11-10 17:50:40 +0530

    pattern 6

---
+++ Loops/Pattern298.java
@@ -0,0 +1,29 @@
+class Pattern298 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = n; i > 0; i--){
+
+            char ch = 'L';
+
+            System.out.println();
+
+            // for spaces
+            for(int k = 0; k < (n-i)/2; k++){
+                System.out.print(" ");
+            }
+
+            // for pattern
+            for(int j = i; j > 0; j--){
+
+                System.out.print(ch);
+
+                ch = (ch == 'L')?'J':'L';
+
+            }
+
+        }
+
+    }
+}
