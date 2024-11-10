f1355f557e6856456f178b92854bb1d124f8a581
PiyushJT <piyushthummar7@gmail.com>
2024-11-10 12:55:51 +0530

    pattern 2

---
+++ Loops/Pattern293.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Pattern293 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+
+        // pattern
+        for(int i = 1; i <= n; i++){
+
+
+            // for spaces
+            for(int k = (n-i)/2; k > 0; k--){
+                System.out.print(" ");
+            }
+
+            // for integer
+            if(i%2 == 1){
+
+                // single line
+                for(int j = 1; j <= i; j++){
+                   System.out.print(j);
+                }
+
+            }
+
+            // for alphabets
+            else{
+                for(char j = 'A'; j <= 64+i; j++){
+                    System.out.print(j);
+                 }
+            }
+            
+            System.out.println();
+
+        }
+
+    }
+}
