7b3995d61f2d2778236fdf581655edf8aea5654c
PiyushJT <piyushthummar7@gmail.com>
2024-11-10 14:38:41 +0530

    pattern 3

---
+++ Loops/Pattern294.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Pattern294 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+
+            System.out.println();
+
+            for(int j = i; j<=n; j++){
+
+                System.out.print(j);
+
+            }
+
+        }
+
+    }
+}
