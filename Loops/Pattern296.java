bacf5166c949613f324747251fdf51aa643e8fba
PiyushJT <piyushthummar7@gmail.com>
2024-11-10 15:02:53 +0530

    pattern 5

---
+++ Loops/Pattern296.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Pattern296 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+
+            System.out.println();
+
+            if(i%2 == 1) {
+                for (int j = 1; j <= i * 2; j += 2) {
+                    System.out.print(j);
+                }
+            }
+            else{
+                for (int j = 2; j <= i * 2; j += 2) {
+                    System.out.print(j);
+                }
+            }
+
+        }
+
+    }
+}
