af686c9ad1fd500f3ed3fbd0d3b24b655b377f95
PiyushJT <piyushthummar7@gmail.com>
2025-06-22 23:31:34 +0530

    File IO programs (all)

--- FileIO/Sem2Program326.java
+++ FileIO/Sem2Program326.java
@@ -15,6 +15,38 @@ public class Sem2Program326 {
 
     public static void main(String[] args) {
 
+        String path = "home/abc/bcd/def/ghi/jkl";
+
+        File dir = new File(path);
+
+        // mkdirS -> with s for multiple
+        dir.mkdirs();
+
+        File newDir = new File("home/abc");
+
+        for (String fileName : newDir.list()) {
+
+            System.out.println();
+            System.out.println();
+
+            File file = new File("home/abc", fileName);
+
+            if (file.isFile())
+                System.out.println("It is a file");
+            else
+                System.out.println("It is a directory");
+
+
+            System.out.println(file.getName() + " -> " + file.length());
+
+            System.out.println("Permissions");
+            System.out.println("Read " + file.canRead());
+            System.out.println("Write " + file.canWrite());
+            System.out.println("Execute " + file.canExecute());
+
+
+        }
+
 
     }
 
